6da84cc2abaa944cdfffaff35f7b8893ab81cc3a
jr <jeanremy.oudin@gmail.com>
2012-05-22 09:54:25 +0200

    Added TMX to the repo + Animations

---
+++ src/com/example/hxmario/Animation.hx
@@ -0,0 +1,79 @@
+package com.example.hxmario;
+
+import nme.display.Sprite;
+import nme.display.Bitmap;
+import nme.display.BitmapData;
+
+import nme.events.Event;
+
+import nme.geom.Point;
+
+import org.flixel.tmx.TmxMap;
+
+class Animation extends Sprite
+{
+	var bitmaps:Array<Bitmap>;
+	var pos:Int;
+	var prev:Int;
+
+	public function new(map:TmxMap, gids:Array<Int>, x:Int, y:Int)
+	{
+		super();
+		this.x = x*map.tileWidth;
+		this.y = y*map.tileHeight;
+		this.width = map.tileWidth;
+		this.height = map.tileHeight;
+
+		trace("this : (" + this.x + ";" + this.y + ")");
+
+		pos = -1;
+		prev = -1;
+		bitmaps = [];
+
+		for (gid in gids)
+			addTile(map, gid);
+
+		addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+	}
+
+	private function onAddedToStage(_:Event):Void
+	{
+		next();
+	}
+
+	public function next():Void
+	{
+		if (prev != -1)
+		#if flash
+			nme.Lib.current.removeChild(bitmaps[prev]);
+		#else
+			removeChild(bitmaps[prev]);
+		#end
+
+		prev = pos;
+		pos++;
+		if (pos >= bitmaps.length)
+			pos = 0;
+		
+		#if flash
+			bitmaps[pos].x = this.x;
+			bitmaps[pos].y = this.y;
+
+			nme.Lib.current.addChild(bitmaps[pos]);
+		#else
+			addChild(bitmaps[pos]);
+		#end
+	}
+
+	private function addTile(map:TmxMap, gid:Int)
+	{
+		var bmpdata = new BitmapData(map.tileWidth, map.tileHeight, false, 0xFF0000);
+		var tileset = map.getGidOwner(gid);
+		if (tileset != null)
+		{
+			var id = tileset.fromGid(gid);
+			bmpdata.copyPixels(tileset.image, tileset.getRect(id), new Point(0, 0));
+			bitmaps.push(new Bitmap(bmpdata));
+		}
+	}
+}
--- src/com/example/hxmario/Mario.hx
+++ src/com/example/hxmario/Mario.hx
@@ -8,18 +8,24 @@ import nme.Assets;
 import nme.Lib;
 import nme.display.StageAlign;
 import nme.display.StageScaleMode;
-
+import nme.utils.Timer;
+import nme.events.TimerEvent;
 import nme.text.TextField;
 import nme.text.TextFormat;
 import nme.text.TextFieldAutoSize;
 import nme.geom.Rectangle;
 import nme.geom.Point;
 
+import org.flixel.tmx.TmxObjectGroup;
 import org.flixel.tmx.TmxMap;
 
 class Mario extends Sprite
 {
 	var map:TmxMap;
+	var animatedTiles:Array<Array<Int>>; // Using GID
+	var staticTiles:Array<Point>; // Using coordinates
+	var objects:Array<Animation>;
+	var timer:Timer;
 
 	public function new()
 	{
@@ -34,28 +40,102 @@ class Mario extends Sprite
 		construct();
 	}
 
+	public function constructAnimatedTiles(group:TmxObjectGroup):Void
+	{
+		var layer = map.layers.get(group.properties.resolve("layer"));
+		for (object in group.objects)
+			if (object.type == "anim")
+			{
+				var tiles = [];
+				var y = Math.floor(object.y/map.tileHeight);
+				for(x in Math.floor(object.x/map.tileWidth)...Math.floor(object.width/map.tileWidth))
+					tiles.push(layer.tileGIDs[y][x]);
+				animatedTiles.push(tiles);
+			}
+	}
+
+	private function readObjects():Void
+	{
+		for(group in map.objectGroups)
+			switch(group.properties.resolve("type"))
+			{
+				case "anim":
+					constructAnimatedTiles(group);
+				default:
+					// Nothing to do :3
+			}
+	}
+
+	private function getAnimation(gid:Int):Null<Array<Int>>
+	{
+		for (array in animatedTiles)
+			for (tile in array)
+				if (tile == gid)
+					return array;
+		return null;
+	}
+
 	private function constructTiles():Void
 	{
 		var layer = map.layers.get("graphics");
-		var bmpdata = new BitmapData(map.width*map.tileWidth, map.height*map.tileHeight);
-		
+		var bmpdata = new BitmapData(map.width*map.tileWidth, map.height*map.tileHeight, false, map.getGidOwner(1).trans);
+
 		for (i in 0...layer.tileGIDs.length)
 			for (j in 0...layer.tileGIDs[i].length)
 			{
-				var tileset = map.getGidOwner(layer.tileGIDs[i][j]);
+				var gid = layer.tileGIDs[i][j];
+				var tileset = map.getGidOwner(gid);
 				if (tileset != null)
 				{
-					var id = tileset.fromGid(layer.tileGIDs[i][j]);
-					bmpdata.copyPixels(tileset.image, tileset.getRect(id), new Point(j*map.tileWidth,i*map.tileHeight));
+					var animation = getAnimation(gid);
+					if (animation == null)
+					{
+						var id = tileset.fromGid(gid);
+						bmpdata.copyPixels(tileset.image, tileset.getRect(id), new Point(j*map.tileWidth,i*map.tileHeight));
+					}
+					else
+					{
+						objects.push(new Animation(map, animation, j, i));
+					}
 				}
 			}
-		addChild(new Bitmap(bmpdata));
+		var bmp = new Bitmap(bmpdata);
+		bmp.cacheAsBitmap = true;
+		addChild(bmp);
+
+		for(object in objects)
+		{
+			//trace(object);
+			nme.Lib.current.addChild(object);
+		}
 	}
 
 	private function construct():Void
 	{
 		map = new TmxMap( Assets.getText("data/map/world1.tmx") );
+		animatedTiles = [];
+		staticTiles = [];
+		objects = [];
+		readObjects();
 		constructTiles();
+		initTimer();
+	}
+
+	private function initTimer():Void
+	{
+		timer = new Timer(120);
+		timer.addEventListener(TimerEvent.TIMER, onTime);
+		timer.start();
+	}
+
+	private function onTime(_:Event):Void
+	{
+		for (object in objects)
+			object.next();
+
+		#if (js || html5)
+			initTimer();
+		#end
 	}
 
 	public static function main():Void
